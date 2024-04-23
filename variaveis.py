395daf7dae3b300edca0c67c3aceb1e557127652
Renan J. A. Silva <renan.junior@docente.senai.br>
2024-04-23 16:38:38 -0300

    criação variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,12 @@
+nome="Renan Silva"
+idade=33
+email='renan.junior@docente.senai.br'
+telefone='17 99999-0000'
+nota=8.99
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)

39adf788817aa3152b9bcf0ffae4f1e2a7aaa426
Renan J. A. Silva <renan.junior@docente.senai.br>
2024-05-02 16:49:16 -0300

    Atualização fstrings2.py

--- fstrings2.py
+++ fstrings2.py
@@ -2,6 +2,10 @@
 escola = 'Senai'
 curso = 'Desenvolvimento de Sistemas'
 uc = 'Lógica de Programação'
+matricula = 34
+nota = 8.999999
 print(f"Escola: {escola}\n"
       f"Curso: {curso}\n"
-      f"Unidade Curricular: {uc}")
+      f"Unidade Curricular: {uc}\n"
+      f"Matrícula: {matricula:06d}\n"
+      f"Nota: {nota:.2f}")

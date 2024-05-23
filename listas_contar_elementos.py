03b21d1980e43a36edf13630906a2e8e92a28779
Renan J. A. Silva <renan.junior@docente.senai.br>
2024-05-23 10:35:27 -0300

    Criação listas_contar_elementos.py

---
+++ listas_contar_elementos.py
@@ -0,0 +1,4 @@
+alunos=['Adriana', 'João', 'Maria', 'Pedro', 'Ana', 'Lucas', 'Mariana', 'Adriana']
+
+ocorrencias = alunos.count('Adriana')
+print(f"Quantidade de ocorrências de Adriana: {ocorrencias}")

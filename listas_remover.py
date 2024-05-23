ae7377050c35d261b47bb082bec0617fc8a8c9bc
Renan J. A. Silva <renan.junior@docente.senai.br>
2024-05-23 09:29:26 -0300

    Criação listas_remover.py

---
+++ listas_remover.py
@@ -0,0 +1,14 @@
+alunos=['Adriana', 'João', 'Maria', 'Pedro', 'Ana', 'Lucas', 'Mariana', 'Adriana']
+print(f"Lista de alunos: {alunos}")
+# Método remove()
+alunos.remove('Adriana')
+print(f"Lista de alunos: {alunos}")
+# Método pop()
+alunos.pop(2)
+print(f"Lista de alunos: {alunos}")
+# Função del
+del alunos[3]
+print(f"Lista de alunos: {alunos}")
+# Método clear()
+alunos.clear()
+print(f"Lista de alunos: {alunos}")
